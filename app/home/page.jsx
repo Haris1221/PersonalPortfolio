7185aa6431757039da884b355711fe28eb0b8c3f
haris <Haris199x@gmail.com>
2023-10-12 00:29:50 -0500

    init

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -50,61 +50,98 @@ export default function Page() {
             initial={{ x:-200, opacity: 0 }}
             whileInView={{ x:0, opacity: 1 }}
             viewport={{ once: true }}
-            transition={ {duration: 1, delay:1}}>
+            transition={ {duration: 1, delay: 0.3}}>
               <Image className='img' src={profile} alt='s'  />
               <p className='about__text'>Really passionate and highly driven who runs by a single code, discipline. I&apos;m A fullstack developer who likes to build things and make them look pleasing to the eye. i like spending time on my computer whether it be for work or playing games with a bunch of friends. mostly the second part though.</p>
             </motion.div>
-            <motion.div className='right flex'  
-            initial={{ x: 200, opacity: 0}}
-            animate={{ x:0, opacity: 1}}
-            transition={{ duration: 1}}>
+            <div className='right flex'>
               <div className='skill'>
-                <div className='content'>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 1.7}}>
                 <Image className='skills__img' src={react} alt='x'/>
                 <p className='skills__name'>React</p>
-                </div>
-                <div className='content'>
+                </motion.div>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 1.5}}>
                 <Image className='skills__img' src={node} alt='x'/>
                 <p className='skills__name'>Node.Js</p>
-                </div>
-                <div className='content'>
+                </motion.div>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 1.3}}>
                 <Image className='skills__img' src={mongo} alt='x'/>
                 <p className='skills__name'>MongoDB</p>
-                </div>
+                </motion.div>
               </div>
               <div className='skill'>
-              <div className='content'>
+              <motion.div className='content' 
+              initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 0.3}}>
               <Image className='skills__img' src={js} alt='x'/>
                 <p className='skills__name'>JavaScript</p>
-              </div>
-                <div className='content'>
+              </motion.div>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 1.9}}>
                 <Image className='skills__img' src={ex} alt='x'/>
                 <p className='skills__name'>Express.Js</p>
-                </div>
-                <div className='content'>
+                </motion.div>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 2.1}}>
                 <Image className='skills__img' src={html} alt='x'/>
                 <p className='skills__name'>HTML</p>
-                </div>
-                <div className='content'>
+                </motion.div>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 1.1}}>
                 <Image className='skills__img' src={css} alt='x'/>
                 <p className='skills__name'>CSS</p>
-                </div>
+                </motion.div>
               </div>
               <div className='skill'>
-              <div className='content'>
+              <motion.div className='content' 
+              initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 0.5}}>
               <Image className='skills__img' src={next} alt='x'/>
                 <p className='skills__name'>Next.Js</p>
-              </div>
-                <div className='content'>
+              </motion.div>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 0.7}}>
                 <Image className='skills__img' src={git} alt='x'/>
                 <p className='skills__name'>Git</p>
-                </div>
-                <div className='content'>
+                </motion.div>
+                <motion.div className='content' 
+                initial={{ opacity: 0 }}
+            whileInView={{opacity: 1 }}
+            viewport={{ once: true }}
+            transition={ {duration: 1, delay: 0.9}}>
                 <Image className='skills__img' src={mongo} alt='x'/>
                 <p className='skills__name'>MongoDB</p>
-                </div>
+                </motion.div>
               </div>
-            </motion.div>
+            </div>
           </div>
           <div>Projects</div>
           <div>Projects</div>
