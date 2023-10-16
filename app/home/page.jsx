fb2fc9a000808c2b852ef2070b6903f2db217271
haris <Haris199x@gmail.com>
2023-10-16 17:16:10 -0500

    fix

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -216,7 +216,7 @@ export default function Page() {
             </div>
             <div className='getInTouch'>
                 <h1 className='section__intro'>Contact</h1>
-                <p className='about__text'>Have a question or want to work together? Leave your details and I'll get back to you as soon as possible.</p>
+                <p className='about__text'>Have a question or want to work together? Leave your details and I&apos;ll get back to you as soon as possible.</p>
                 <Contact />
                 </div>
                 <footer className='footer'>
